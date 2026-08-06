aa535ec52a163c382af5b03d6b313ebd77b8f39d
TDWolff <torin.dean.wolff@gmail.com>
2026-08-06 12:15:24 -0700

    Initial UCAL SwiftUI app project
    Add initial Xcode project and SwiftUI app scaffold for UCAL. Includes .gitignore, project files (project.pbxproj, workspace), asset catalogs (AppIcon, AccentColor, Assets.xcassets), app entry UCALApp.swift, a basic ContentView.swift (Hello World), and user workspace UI state. Sets up project configuration and basic app resources for further development.

---
+++ UCAL/UCALApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCALApp.swift
+//  UCAL
+//
+//  Created by Torin Wolff on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct UCALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
